48f763b43f5fca9ac03ba5467d0c1949e8881587
Said El Dah <saeed.eldah@gmail.com>
2016-09-14 11:39:07 +0300

    My First Linq App

---
+++ LinqExample/Book.cs
@@ -0,0 +1,9 @@
+﻿namespace LinqExample
+{
+    class Book
+    {
+        public string Title { get; set; }
+        public float Price { get; set; }
+
+    }
+}
--- LinqExample/Program.cs
+++ LinqExample/Program.cs
@@ -6,10 +6,47 @@ using System.Threading.Tasks;
 
 namespace LinqExample
 {
-    class Program
+    partial class Program
     {
         static void Main(string[] args)
         {
+            var bookList = GetBookList();
+
+            //LINQ Query Operator
+            var bookList1 = from b in bookList
+                where b.Price < 100 
+                orderby b.Price 
+                select b
+            ;
+            foreach (Book book in bookList1)
+            {
+                Console.WriteLine(book.Title + " : " + book.Price);
+            }
+            //LinqExample Extension Methods
+            var bookTitleList = bookList
+                .Where(b => b.Price < 100)
+                .OrderBy(b => b.Title)
+                .Select(b => b.Title);
+
+            foreach (var bookTitle in bookTitleList)
+            {
+                Console.WriteLine(bookTitle);
+            }
+     
+        }
+
+        static IEnumerable<Book> GetBookList()
+        {
+            return new List<Book>()
+            {
+                new Book() {Title = "gege", Price = 50},
+                new Book() {Title = "A", Price = 90},
+                new Book() {Title = "sasa", Price = 100},
+                new Book() {Title = "mama", Price = 500},
+                new Book() {Title = "Ahmad", Price = 200},
+                new Book() {Title = "B", Price = 30},
+                new Book() {Title = "SAeed", Price = 20},
+            };
         }
     }
 }
